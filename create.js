cf7bf20f799757974d3d6e59c3819737e8227f8d
nks-19 <nehakumarisingh1904@gmail.com>
2024-04-21 03:32:22 +0530

    Adding our Serverless API

--- create.js
+++ create.js
@@ -1,6 +1,7 @@
 import * as uuid from "uuid";
 import handler from "./libs/handler-lib";
 import dynamoDb from "./libs/dynamodb-lib";
+
 export const main = handler(async (event, context) => {
     const data = JSON.parse(event.body);
     const params = {
